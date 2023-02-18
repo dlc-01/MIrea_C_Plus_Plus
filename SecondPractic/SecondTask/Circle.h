63baeccbfbab961490f8f794043977cbe6d5ae3a
DLC <55656405+dlc-01@users.noreply.github.com>
2023-02-18 22:49:57 +0300

    Create Circle.h

---
+++ SecondPractic/SecondTask/Circle.h
@@ -0,0 +1,19 @@
+#ifndef CIRCLE_CIRCLE_H
+#define CIRCLE_CIRCLE_H
+
+class Circle {
+private:
+    float radius;
+    float x_center;
+    float y_center;
+
+public:
+    Circle(float r, float x, float y);
+    void set_circle(float r, float x, float y);
+    float square();
+    bool triangle_around(float a, float b, float c);
+    bool triangle_in(float a, float b, float c);
+    bool check_circle(float r, float x_cntr, float y_cntr);
+};
+
+#endif //CIRCLE_CIRCLE_H
